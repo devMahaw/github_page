5905e42ab8957f6bb4f290440d4f0f2232e0b69a
Marcelo Dantas <mahawprogramacao@gmail.com>
2023-11-17 13:08:46 -0300

    project setup

---
+++ src/styles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const EstiloGlobal = createGlobalStyle`
+  * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+    font-family: sans-serif;
+  }
+`;
+
+export default EstiloGlobal;
